9c1635764e8843fce1b61b06b96bf1b0f9c8fb0e
Irina Bright <missirinabright@gmail.com>
2019-05-30 02:45:11 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,9 +20,8 @@ def bonus
    }
   }
 
- epic_tragedy[:montague][:hero][:status] = "dead"
- 
-puts epic_tragedy
+epic_tragedy[:montague][:hero][:status] = "dead"
+
 
 epic_tragedy[:capulet][:heroine][:status] = "dead"
  
